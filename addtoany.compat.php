6befeeb75e3530f802056c3e1cab60f3da1f0ff6
Ignacio Abejaro <ignacio.abejaro@ticxcat.cat>
2020-02-14 12:52:22 +0100

    added xtec changes

--- addtoany.compat.php
+++ addtoany.compat.php
@@ -25,10 +25,11 @@ function addtoany_excerpt_remove() {
 /**
  * Load AMP (Accelerated Mobile Pages) compatibility functions
  */
+
 add_action( 'amp_post_template_css', 'addtoany_amp_additional_css_styles' );
 
 function addtoany_amp_additional_css_styles( $amp_template ) {
-	// only CSS here please...
+	// CSS only
 	?>
 	.addtoany_list a {
 		padding: 0 4px;
@@ -39,7 +40,54 @@ function addtoany_amp_additional_css_styles( $amp_template ) {
 	<?php
 }
 
-function addtoany_amp_icons_css( $amp_template ) {
-	global $A2A_SHARE_SAVE_amp_icons_css;
-	echo $A2A_SHARE_SAVE_amp_icons_css;
+/**
+ * Change the priority of standard buttons in content to work around a
+ * Jetpack ~v7.8 Related Posts bug that removes content added to AMP posts 
+ * if the content's filter has a priority number greater than 40.
+ */
+add_action( 'wp_loaded', 'addtoany_priority_for_amp_jetpack' );
+
+function addtoany_priority_for_amp_jetpack() {
+	// If the AMP plugin is enabled, the Jetpack plugin is enabled,
+	// and Jetpack's Related Posts module is enabled
+	if ( class_exists( 'AMP_Autoloader' ) && class_exists( 'Jetpack' ) && Jetpack::is_module_active( 'related-posts' ) ) {
+		// Change priority to 20
+		add_filter( 'addtoany_content_priority', function() { return 20; } );
+	}
+}
+
+/**
+ * Move buttons from WooCommerce product description to WooCommerce's sharing block
+ */
+add_action( 'woocommerce_share', 'addtoany_woocommerce_share', 10 );
+
+function addtoany_woocommerce_share() {
+	remove_filter( 'the_content', 'A2A_SHARE_SAVE_add_to_content', 98 );
+	remove_filter( 'the_excerpt', 'A2A_SHARE_SAVE_add_to_content', 98 );
+	
+	$options = get_option( 'addtoany_options', array() );
+	$sharing_disabled = get_post_meta( get_the_ID(), 'sharing_disabled', true );
+	$sharing_disabled = apply_filters( 'addtoany_sharing_disabled', $sharing_disabled );
+	$post_type = get_post_type( get_the_ID() );
+	
+	if ( 
+		// Private post
+		get_post_status( get_the_ID() ) == 'private' ||
+		// Sharing disabled on post
+		! empty( $sharing_disabled ) ||
+		// Custom post type (usually "product") disabled
+		( $post_type && isset( $options['display_in_cpt_' . $post_type] ) && $options['display_in_cpt_' . $post_type] == '-1' )
+	) {
+		return;
+	} else {
+		// If a Sharing Header is set
+		if ( ! empty( $options['header'] ) ) {
+			echo '<div class="addtoany_header">' . stripslashes( $options['header'] ) . '</div>';
+		} else {
+			$html_header = '';
+		}
+		
+		// Display share buttons
+		ADDTOANY_SHARE_SAVE_KIT();
+	}
 }
